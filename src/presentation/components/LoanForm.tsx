bb29d5ccb870d55fa04442be27298b4c179ff652
Yesler2113 <diazmunozdm12@gmail.com>
2024-07-27 01:54:44 -0600

    validations

--- src/presentation/components/LoanForm.tsx
+++ src/presentation/components/LoanForm.tsx
@@ -1,76 +1,127 @@
-// LoanForm.tsx
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Pressable } from 'react-native';
-import { LoanContext } from '../hooks/useLoanCalculator';
+import React, {useState, useContext} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Alert,
+  ScrollView,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {LoanContext} from '../hooks/useLoanCalculator';
 
 export const LoanForm = () => {
   const [amount, setAmount] = useState('');
   const [annualInterestRate, setAnnualInterestRate] = useState('');
   const [termInYears, setTermInYears] = useState('');
   const [disbursementDate, setDisbursementDate] = useState('');
+  const [error, setError] = useState('');
 
   const loanContext = useContext(LoanContext);
+  const navigation = useNavigation();
 
   if (!loanContext) {
-    return null; // o maneja el error de manera adecuada
+    return null;
   }
 
-  const { calculateLoan } = loanContext;
+  const {calculateLoan} = loanContext;
 
   const handleSubmit = () => {
+    if (!amount || !annualInterestRate || !termInYears || !disbursementDate) {
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+
     const loanDetails = {
       amount: parseFloat(amount),
       annualInterest: parseFloat(annualInterestRate),
       period: parseInt(termInYears, 10),
-      date: new Date(disbursementDate)
+      date: new Date(disbursementDate),
     };
 
-    // Llama a la función calculateLoan del contexto
     calculateLoan(loanDetails);
+
+    setAmount('');
+    setAnnualInterestRate('');
+    setTermInYears('');
+    setDisbursementDate('');
+    setError('');
+
   };
 
   return (
-    <View style={{}}>
-      <Text style={{ color: 'black', marginLeft:15 }}>Monto:</Text>
-      <TextInput
-        value={amount}
-        onChangeText={setAmount}
-        keyboardType="numeric"
-        style={{ backgroundColor: 'grey', borderRadius: 10, borderColor: 'black', borderWidth: 1, margin: 15}}
-      />
-      <Text style={{ color: 'black', marginLeft:15 }}>Tasa de Interés Anual:</Text>
-      <TextInput
-        value={annualInterestRate}
-        onChangeText={setAnnualInterestRate}
-        keyboardType="numeric"
-        style={{ backgroundColor: 'grey', borderRadius: 10, borderColor: 'black', borderWidth: 1, margin: 15 }}
-      />
-      <Text style={{ color: 'black', marginLeft:15 }}>Plazo (años):</Text>
-      <TextInput
-        value={termInYears}
-        onChangeText={setTermInYears}
-        keyboardType="numeric"
-        style={{ backgroundColor: 'grey', borderRadius: 10, borderColor: 'black', borderWidth: 1, margin: 15 }}
-      />
-      <Text style={{ color: 'black', marginLeft:15 }}>Fecha de Desembolso:</Text>
-      <TextInput
-        value={disbursementDate}
-        onChangeText={setDisbursementDate}
-        style={{ backgroundColor: 'grey', borderRadius: 10, borderColor: 'black', borderWidth: 1, margin: 15 }}
-      />
-      <Pressable onPress={handleSubmit} style={{ borderColor: 'green', alignItems: 'center', backgroundColor:'green', borderRadius: 20,marginTop: 20}}>
-        <Text style={{color:'black', fontSize:30, }}>Calcular</Text>
-      </Pressable>
-    </View>
+    <ScrollView>
+      <View>
+        <Text style={{color: 'black', marginLeft: 15}}>Monto:</Text>
+        <TextInput
+          value={amount}
+          onChangeText={setAmount}
+          keyboardType="numeric"
+          style={{
+            backgroundColor: 'grey',
+            borderRadius: 10,
+            borderColor: 'black',
+            borderWidth: 1,
+            margin: 15,
+          }}
+        />
+        <Text style={{color: 'black', marginLeft: 15}}>
+          Tasa de Interés Anual:
+        </Text>
+        <TextInput
+          value={annualInterestRate}
+          onChangeText={setAnnualInterestRate}
+          keyboardType="numeric"
+          style={{
+            backgroundColor: 'grey',
+            borderRadius: 10,
+            borderColor: 'black',
+            borderWidth: 1,
+            margin: 15,
+          }}
+        />
+        <Text style={{color: 'black', marginLeft: 15}}>Plazo (años):</Text>
+        <TextInput
+          value={termInYears}
+          onChangeText={setTermInYears}
+          keyboardType="numeric"
+          style={{
+            backgroundColor: 'grey',
+            borderRadius: 10,
+            borderColor: 'black',
+            borderWidth: 1,
+            margin: 15,
+          }}
+        />
+        <Text style={{color: 'black', marginLeft: 15}}>
+          Fecha de Desembolso:
+        </Text>
+        <TextInput
+          value={disbursementDate}
+          onChangeText={setDisbursementDate}
+          style={{
+            backgroundColor: 'grey',
+            borderRadius: 10,
+            borderColor: 'black',
+            borderWidth: 1,
+            margin: 15,
+          }}
+        />
+        {error ? (
+          <Text style={{color: 'red', marginLeft: 15}}>{error}</Text>
+        ) : null}
+        <Pressable
+          onPress={handleSubmit}
+          style={{
+            borderColor: 'green',
+            alignItems: 'center',
+            backgroundColor: 'green',
+            borderRadius: 20,
+            marginTop: 20,
+          }}>
+          <Text style={{color: 'black', fontSize: 30}}>Calcular</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
   );
 };
-
-
-
-
-
-
-
-
-
-
